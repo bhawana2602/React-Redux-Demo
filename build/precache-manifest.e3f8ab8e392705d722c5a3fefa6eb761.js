6ee066b2868e0249146b4aa17d629a128b7972db
Bhawana Parashar <bparasha@in.ibm.com>
2020-08-11 12:27:38 +0530

    Progressive web app

---
+++ build/precache-manifest.e3f8ab8e392705d722c5a3fefa6eb761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c9197cc56e0ecec0549cb131ac1d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ada8007a45a0010123",
+    "url": "/static/css/main.02949905.chunk.css"
+  },
+  {
+    "revision": "e57c7223026c3b128a20",
+    "url": "/static/js/2.6a2bc4c5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6a2bc4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ada8007a45a0010123",
+    "url": "/static/js/main.cf1ec151.chunk.js"
+  },
+  {
+    "revision": "e81f640af46508537cbc",
+    "url": "/static/js/runtime-main.35caad74.js"
+  }
+]);
